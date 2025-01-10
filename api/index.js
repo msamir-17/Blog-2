1581f4007a4c1098d2e1eba75570faa2165107ec
technosolz <technosolz01@gmail.com>
2025-01-10 16:33:35 +0530

    Creating Mern Blog Site

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose' ;
+import dotenv from 'dotenv';
+import userRoutes from './route/user-route.js';
+import authRoute from './route/auth-route.js';
+
+dotenv.config();
+
+
+mongoose
+    .connect(process.env.MONGO )
+        .then(() => {
+            console.log('mongosdb is connected');
+        }).catch(err => {
+            console.log(err);
+        });
+    
+    
+
+const app = express();
+
+app.use(express.json())
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000!");
+});
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoute)
---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Project from './pages/Project'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/projects" element={<Project/>} ></Route>
+        <Route path="/about" element={<About/>} ></Route>
+        <Route path="/sign-in" element={<Signin/>} ></Route>
+        <Route path="/sign-up" element={<Signup/>} ></Route>
+        <Route path="/dashboard" element={< Dashboard />} ></Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
