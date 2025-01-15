5d2ff201bac8e52d9224a0a257aabe220c61db65
technosolz <technosolz01@gmail.com>
2025-01-15 23:32:21 +0530

    Adding Functionality To The Sign Up Page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,23 +1,46 @@
-import { Button, Label, TextInput } from 'flowbite-react';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 // import e from "express";
 
 const Signup = () => {
   const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage ] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) =>{
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
 const handleSubmit = async (e) => {
   e.preventDefault();
+  if(!formData.username || !formData.email || !formData.password){
+    return setErrorMessage('Please Fill Out The All Fields');
+  }
   try{
-    const response = await fetch('')
+    setLoading(true);
+    setErrorMessage(null);
+    const response = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {'Content-Type' : ' application/json' },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    if(data.success === false){
+      return setErrorMessage(data.message);
+    }
+    setLoading(false);
+    if(response.ok){
+      navigate('/sign-in');
+    }
   } catch(error){
-
+    setErrorMessage(error.message);
+    setLoading(false);
   }
 };
+
+
   return (
     <div className='min-h-screen mt-20' >
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 ">
@@ -53,13 +76,30 @@ const handleSubmit = async (e) => {
               <TextInput type='password' placeholder='Password' id='password' onChange={handleChange} />                       
             </div>
 
-            <Button gradientDuoTone="redToYellow" type='submit' >Sign Up</Button>
+            <Button gradientDuoTone="redToYellow" type='submit' disabled={loading} >
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm'/>
+                  <span className='pl-3' >Loading...</span>
+                  </>                
+                  
+                ) : ('Sign Up')              
+              }
+            </Button>
           </form>
 
           <div className=" flex gap-2 text-sm mt-5">
             <span>  Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>SignIn</Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure' >
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   server:{
-//     proxy:{
-//       '/api':{
-//         target: 'http://localhost:3000',
-//         secure: false,
-//       },
-//     },
-//   },
-//   plugins: [react()],
-// })
+
+
+
 export default defineConfig({
-  plugins: [react()],
+  server:{
+    proxy:{
+      '/api':{
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()], 
 })
