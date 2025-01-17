de9c8b1cbd453adcf8e7fca9220bf6fce28a3f0a
technosolz <technosolz01@gmail.com>
2025-01-17 19:28:59 +0530

    Create SignIn Api Route

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -47,10 +47,12 @@ export const signin = async (req , res , next) => {
         }
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
 
-        const {password: pass  , ...rest} = validUser._doc;
+        // const {password: pass, ...rest} = validUser._doc;
+        const { password: pass, ...rest } = validUser._doc;
 
-        res.status(200).cookie('access_token', token,{
-            httpOnly: true,}).json(rest);
+        res.status(200)
+            .cookie('access_token', token, { httpOnly: true })
+            .json(rest);
         
 
     } catch(error){
